183e741c2c78d2402eafd9e92ba107dd50d70f53
Csaba Trucza <csaba.trucza@iquestgroup.com>
2014-07-30 08:30:12 +0300

    First attempt at representing the board
    For now it's just a hard-coded array of ints.

--- Battleship/BattleshipPuzzle.cs
+++ Battleship/BattleshipPuzzle.cs
@@ -2,12 +2,25 @@ namespace Battleship
 {
     public class BattleshipPuzzle
     {
+        private int[,] board = new int[10,10];
+
+        public BattleshipPuzzle()
+        {
+            AddShip(0, 1, 1);
+        }
+
+        private void AddShip(int x, int y, int size)
+        {
+            for (int i = 0; i < size; ++i)
+                board[x, y + i] = 1;
+        }
+
         public Result ShootAt(int x, int y)
         {
-            if (y==1)
-                return Result.Hit;
-            else
+            if (board[x, y] == 0)
                 return Result.Miss;
+            else
+                return Result.Hit;
         }
     }
 }
