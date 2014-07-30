2dcab28804ee4627b4b5a532066271f9e5270c63
Csaba Trucza <csaba.trucza@iquestgroup.com>
2014-07-30 08:20:31 +0300

    Clean up unit tests

--- BattleshipTests/BattleshipPuzzleTests.cs
+++ BattleshipTests/BattleshipPuzzleTests.cs
@@ -6,26 +6,25 @@ namespace BattleshipTests
     [TestFixture]
     internal class BattleshipPuzzleTests
     {
-        [Test]
-        public void Test_usage()
+        private static BattleshipPuzzle puzzle;
+
+        [SetUp]
+        public void CreateNewPuzzle()
         {
-            var battleship = new BattleshipPuzzle();
-            battleship.ShootAt(0, 0);
+            puzzle = new BattleshipPuzzle();
         }
 
         [Test]
         public void Shooting_in_water_misses()
         {
-            var battleship = new BattleshipPuzzle();
-            var result = battleship.ShootAt(0, 0);
+            Result result = puzzle.ShootAt(0, 0);
             Assert.AreEqual(Result.Miss, result);
         }
 
         [Test]
         public void Shooting_at_a_ship_hits_it()
         {
-            var battleship = new BattleshipPuzzle();
-            var result = battleship.ShootAt(0, 1);
+            Result result = puzzle.ShootAt(0, 1);
             Assert.AreEqual(Result.Hit, result);
         }
 
